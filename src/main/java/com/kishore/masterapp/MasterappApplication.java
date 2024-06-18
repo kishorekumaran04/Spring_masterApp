d871eea1a2cad335218e149ee300b9c72c7ffe12
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-06-18 12:00:54 -0400

    first commit

---
+++ src/main/java/com/kishore/masterapp/MasterappApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.masterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterappApplication.class, args);
+	}
+
+}
