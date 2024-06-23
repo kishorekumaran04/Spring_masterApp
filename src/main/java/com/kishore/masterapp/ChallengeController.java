3494b1aa771ef70f2fdd30568938fed5e7c5164a
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-06-23 16:57:35 -0400

    updating database to mySQL

--- src/main/java/com/kishore/masterapp/ChallengeController.java
+++ src/main/java/com/kishore/masterapp/ChallengeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/challenges")
+@CrossOrigin(origins = {"http://localhost:5173", 
+		"http://challenge-app-live.s3-website.us-east-2.amazonaws.com/"})
 public class ChallengeController {
 
 	private ChallengeService challengeService;
